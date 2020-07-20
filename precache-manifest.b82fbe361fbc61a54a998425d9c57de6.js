0c6672dbaed3298cf349e28d57bc76dde9b5c6ff
redwan awadi <bnitamoublida47@gmail.com>
2020-07-20 18:48:39 +0100

    Updates

---
+++ precache-manifest.b82fbe361fbc61a54a998425d9c57de6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e7cb2b610e1407c0915e22f52957531",
+    "url": "/GitHub_Finder_47/index.html"
+  },
+  {
+    "revision": "4f91da1c8339ff32339f",
+    "url": "/GitHub_Finder_47/static/css/main.53542d42.chunk.css"
+  },
+  {
+    "revision": "d0a4fc113d076f3ade6a",
+    "url": "/GitHub_Finder_47/static/js/2.5840d986.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GitHub_Finder_47/static/js/2.5840d986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f91da1c8339ff32339f",
+    "url": "/GitHub_Finder_47/static/js/main.0d59ff67.chunk.js"
+  },
+  {
+    "revision": "97a2f83ce8111d288e0d",
+    "url": "/GitHub_Finder_47/static/js/runtime-main.ce1a5efa.js"
+  },
+  {
+    "revision": "ac4471291c620d8dd47697a1d8da4975",
+    "url": "/GitHub_Finder_47/static/media/spinner.ac447129.gif"
+  }
+]);
